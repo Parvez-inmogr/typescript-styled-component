db4e43d31427f0fea72abee3c18eb169b7ba937d
hparvez905 <hparvez905@gmail.com>
2022-12-09 14:25:51 +0600

    animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Animation } from './component/Animation';
 import { Header } from './component/Header';
 import { Hero } from './component/Hero';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Hero></Hero>
+      <Animation name='Parvez' isLoggedIn={false} messageCount={10}></Animation>
      
     </div>
   );
--- src/component/Animation.tsx
+++ src/component/Animation.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
 
-export const Animation = () => {
+type Data={
+    name:string;
+    messageCount:number;
+    isLoggedIn:boolean;
+}
+export const Animation = (props:Data) => {
   return (
-    <div>Animation</div>
+    <div>
+       <h2>{props.isLoggedIn ? `  Welcome ${props.name} ! you have ${props.messageCount} notiofication`:'Welcome Guest '}</h2>
+
+      
+    </div>
   )
 }
